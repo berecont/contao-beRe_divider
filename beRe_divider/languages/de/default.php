c5408e4af4234c758c4a8c8f26e126fa4373983f
Bernhard Renner <info@berecont.at>
2017-11-05 17:39:23 +0100

    Create default.php

---
+++ beRe_divider/languages/de/default.php
@@ -0,0 +1,20 @@
+<?php 
+
+/**
+ * Contao Open Source CMS
+ * 
+ * Copyright (C) 2005-2013 Leo Feyer
+ * 
+ * @package   dividers
+ * @author    Bernhard Renner
+ * @license   GPL
+ * @copyright Bernhard Renner 2017
+ */
+
+
+/**
+ * Back end modules dividers - Content Elements
+ */
+$GLOBALS['TL_LANG']['CTE']['beRe_Elemente'] = array('beRecont');
+$GLOBALS['TL_LANG']['CTE']['beRe_trennlinie'] = array('Trennlinie');
+
