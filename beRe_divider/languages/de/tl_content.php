505a5fcd4cdd6618d522be1969a7ebb1190e1335
Bernhard Renner <info@berecont.at>
2017-11-05 17:39:51 +0100

    Create tl_content.php

---
+++ beRe_divider/languages/de/tl_content.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * Contao Open Source CMS
+ * Copyright (c) 2005-2015 Leo Feyer
+ *
+ * @package   dividers
+ * @author    Bernhard Renner
+ * @license   GPL
+ * @copyright Bernhard Renner 2017
+ */
+
+
+/**
+ * table: tl_content
+ */
+$strName ='tl_content';
+
+/**
+ * Fields
+ */
+$GLOBALS['TL_LANG'][$strName]['beRe_divider_type'] = array('Trennlinie','Trennlinie/divider/hr auswählen');
+
+/**
+ * Legends
+ */
+$GLOBALS['TL_LANG'][$strName]['beRe_trennlinie'] = 'Trennlinie/Divider';
+$GLOBALS['TL_LANG'][$strName]['divider01'] = 'Trennlinie Typ 1 [50% middle 3px]';
+$GLOBALS['TL_LANG'][$strName]['divider02'] = 'Trennlinie Typ 2 [50% middle 1px]';
+$GLOBALS['TL_LANG'][$strName]['divider03'] = 'Trennlinie Typ 3 [100% 1px]';
+$GLOBALS['TL_LANG'][$strName]['divider04'] = 'Trennlinie Typ 4 [100% 1px invisible]';
